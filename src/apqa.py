added WMC gradients, fixed bugs, added last-step

src/apqa.py
@@ -1,31 +1,32 @@
 import glog
 import json
-from embedding.rule_parser import RuleParser
+from rule_parser import RuleParser
 from kge.model import KgeModel
 from kge.config import Config
 from kge.dataset import Dataset
 from kge.util.io import load_checkpoint
-from embedding.embtopk.embtopktable import EmbTopKEDBTable
-from wmc import WMC
-from utils import  get_terms_from_body, parse_program, get_query_id_form_query
+from embtopk.embtopktable import EmbTopKEDBTable
+from wmc.sdd_wmc import WMCSolver
+from utils import  get_terms_from_body, parse_program, get_query_id_form_query, get_py_format, print_proof
 import logging
 import torch
 import os
 from tqdm import tqdm
+from collections import defaultdict
+
 class Query():
     """Abstract representation of the (datalog) Queries specific to CQD/QTO (Hamilton)"""
-    #TODO REWRITE THIS CLASS
     def __init__(self, datalog_file, query_type):
         try:
             # to parser
             self._query_type = query_type
             self._datalog_program, query, rule_1, rule_2, rule_3 = parse_program(datalog_file, query_type)
-            print("QUERY", query)
+
             self._qid = get_query_id_form_query(query)
             self._predicates_in_query, self._subjects_in_query = get_terms_from_body(query)
             self._predicates_in_rule_1, self._subjects_in_rule_1 = get_terms_from_body(rule_1)
-            self._predicates_in_rule_2, self._subjects_in_rule_1 = get_terms_from_body(rule_2)
-            self._predicates_in_rule_3, self._subjects_in_rule_1 = get_terms_from_body(rule_3)
+            self._predicates_in_rule_2, self._subjects_in_rule_2 = get_terms_from_body(rule_2)
+            self._predicates_in_rule_3, self._subjects_in_rule_3 = get_terms_from_body(rule_3)
 
         except Exception as e:
             raise ValueError(f"Incorrect Datalog rule or query type format, error: {e}")
@@ -84,7 +85,7 @@ class Query():
 
     @property
     def first_subject_rule_3(self):
-        return self._subejcts_in_rule_3[0]
+        return self._subjects_in_rule_3[0]
 
 
 
@@ -113,77 +114,51 @@ class Engine():
         return self._querier.get_term_name(term)
 
     def _decode_determnistic_fact(self, atom):
+
         _, subject_id, predicate_id, object_id = atom 
         return ( self._decode_term(predicate_id), self._decode_term(subject_id), self._decode_term(object_id))
 
     def _decode_probabilistic_fact(self, atom):
         predicate_id, subject_id, object_id, probability = atom 
+
         return (self.querier.get_predicate_name(predicate_id), self._decode_term(subject_id), self._decode_term(object_id)), float(self._decode_term(probability))
 
-    def get_facts_in_predicate(self, predicate):
-        results = []
-        
-        nodes = self._querier.get_node_details_predicate(f"{predicate}")
-        nodes = json.loads(nodes)
-        
-        for node in nodes:
-            facts = self._querier.get_facts_in_TG_node(int(node['id']))
-            facts = json.loads(facts)
-            for fact in facts:
-                try:
-                    name, prob = fact
-                    print(name, prob)
-                    
-                    if float(prob) > 0.01: 
-                        results.append(name)
-                                         
-                except ValueError:
-                    results.append(fact)
-        
-        return results
-
-
-
-    # Rewrite this one
-    def get_lineage(self, query):
+    def get_lineage(self, query, dataset):
         probabilities ={}
-
-        answers = {}
-        variables_per_answer = {}
+        contains_deteremnistic_answer = defaultdict(list)
+        answers = defaultdict(list)
+        variables_per_answer = defaultdict(set)
         nodes = self._querier.get_node_details_predicate(query.qid)
         if nodes == "{}":
-            return query.qid, ({}, {}), {}
-        
+            return query.qid, ({}, {}, {}), {}
         tuples = self._querier.get_facts_coordinates_with_predicate(query.qid)
-        for current_answer, coordinates in tuples:
-            current_answer = self._decode_term(current_answer[0])
-            node, factid = coordinates
-            leaves = self._querier.get_leaves(node, factid)
+
+        for current_answer, (node, fact_id) in tuples:
             
-            answers[current_answer] = list()
-            variables_per_answer[current_answer] = set()
+            decoded_answer = self._decode_term(current_answer[0])
+            leaves = self._querier.get_leaves(node, fact_id)
+            # print("CA",decoded_answer, dataset.entitites_to_name[decoded_answer.strip("<>")], leaves)
+  
 
             for l in leaves:
                 proof = list()
+                
+                proof_is_determenistic = True
                 for leaf in l:
                     if self._is_determenistic_fact(leaf):
                         fact = self._decode_determnistic_fact( leaf)
                     else:
                         fact, probability = self._decode_probabilistic_fact( leaf)
                         probabilities[fact] = probability
-
-                    proof.append(fact)
+                        proof_is_determenistic = False
                     
-                answers[current_answer].append(proof)
-                variables_per_answer[current_answer].update(proof)
-        return query.qid, (answers, variables_per_answer), probabilities
+                    proof.append(fact)
 
-
-def kbc_to_kge(kge_model_dict, kbc_model_dict):
-    kge_model_dict['model'][0]['_entity_embedder.embeddings.weight'] = kbc_model_dict['model_state_dict']["embeddings.0.weight"]
-    kge_model_dict['model'][1]['_relation_embedder.embeddings.weight'] = kbc_model_dict['model_state_dict']["embeddings.1.weight"]
-
-    return kge_model_dict
+        
+                answers[decoded_answer].append(proof)
+                contains_deteremnistic_answer[decoded_answer].append(proof_is_determenistic)
+                variables_per_answer[decoded_answer].update(proof)
+        return query.qid, (answers, variables_per_answer, contains_deteremnistic_answer), probabilities
 
 
 class ApproximateProbabilisticQueryAnswerer():
@@ -196,16 +171,14 @@ class ApproximateProbabilisticQueryAnswerer():
     
     """
 
-    def __init__(self, embedding_model_path, edb_config_path, k, dataset):
+    def __init__(self, embedding_model_path, edb_config_path, k, threshold, max_queries, dataset):
         self._edb = glog.EDBLayer(edb_config_path)
         self._program = glog.Program(self._edb)
         self._rules = []
         self._k = k
-        checkpoint = load_checkpoint(embedding_model_path)
-        # checkpoint_kbc = load_checkpoint("/home/yannick/phd/coding/cqd/models/FB15k-237-model-rank-500-epoch-100-1602506111.pt")
-        # checkpoint = kbc_to_kge(checkpoint_kge, checkpoint_kbc)
-      
-        self._embedding_model = KgeModel.create_from(checkpoint)
+        self._threshold = threshold
+        self._max_queries = max_queries
+        self._embedding_model = KgeModel.create_from(load_checkpoint(embedding_model_path))
         
         self._dataset = dataset
 
@@ -233,6 +206,14 @@ class ApproximateProbabilisticQueryAnswerer():
     def k(self):
         return self._k
     
+    @property
+    def threshold(self):
+        return self._threshold
+    
+    @property
+    def max_queries(self):
+        return self._max_queries
+    
     @program.setter
     def program(self, edb):
         self._program = glog.Program(edb)
@@ -240,9 +221,6 @@ class ApproximateProbabilisticQueryAnswerer():
 
 
     def answer_queries(self, query_type):
-        solver = WMC()
-        results = {}
-        query_scores = torch.zeros(self.dataset.num_entities)
         final_scores = None
 
         os.chdir(self.dataset.datalog_queries_path)
@@ -250,20 +228,25 @@ class ApproximateProbabilisticQueryAnswerer():
         query_list.sort(key=lambda x: int(x))
 
         for query_id in tqdm(query_list):
+            query_scores = torch.zeros(self.dataset.num_entities)
+
             query_path = self.dataset.get_query_path(query_type, query_id)
-            if query_id == "3":
-                return final_scores
             try:
+
+                if query_id == str(self.max_queries):
+                    return final_scores
+           
+
                 qid, answers, probabilities = self._answer_query(query_path, query_type)
                 qid = qid.replace("Q", "")
-                
-                wmc = solver.solve(answers, probabilities)
-                results[qid] = wmc
-                # print("wmc", wmc)
+                solver = WMCSolver(answers, probabilities)
+                wmc, _ = solver.get_wmcs()
 
-                entity_indexes = [self.dataset.entities_to_index[entity.strip("<>")] for entity in wmc.keys()]
-                query_scores[entity_indexes] = torch.Tensor(list(wmc.values()))
-                final_scores = query_scores if final_scores is None else torch.column_stack([final_scores,query_scores])
+                entity_indexes = [(self.dataset.entities_to_index[entity.strip("<>")]) for entity in wmc.keys()]
+                query_scores[entity_indexes] = torch.Tensor([score for score, _ in wmc.values()])
+                query_scores = query_scores.unsqueeze(0)
+
+                final_scores = query_scores if final_scores is None else torch.cat((final_scores, query_scores))
 
             except Exception as e:
                 logging.error(e)
@@ -283,13 +266,13 @@ class ApproximateProbabilisticQueryAnswerer():
 
         for rule in query.datalog_program:
             self.program.add_rule(rule.strip("\n"))
+
         engine = Engine(self.edb, self.program)
-        return engine.get_lineage(processed_query)
+        return engine.get_lineage(processed_query, self.dataset)
     
 
 
     def _preprocess(self, query):
-      
         
         if query.type == "1_2":
             return self._handle_2p(query)
@@ -318,52 +301,88 @@ class ApproximateProbabilisticQueryAnswerer():
             raise Exception(f"Query type {query.type} not supported!")
 
 
-    def _handle_atom(self, first_predicate, first_constant):
-        self._add_top_k_source(first_predicate, first_constant)
-    
+    def _add_emb_source(self, predicate):
+        print("adding source", predicate)
+        
+        E = EmbTopKEDBTable(predicate, predicate, self.k, self.edb, self.embedding_model, self.dataset.entity_dict, score_threshold=self.threshold)
+        self.edb.add_source(predicate, E)        
 
-    def _handle_conjunction(self, first_predicate, first_constant, second_predicate):
-        print("FP", first_predicate)
-        print("SP", first_constant)
+        return E
 
-        self._add_top_k_source(first_predicate, first_constant)
-        intermediate_variable_assignments = self._collect_top_k_intermediate_variable_assignments(first_predicate, first_constant)
+
+    def _handle_conjunction(self, first_predicate, first_subject, second_predicate):
         
-        for possible_assignment in intermediate_variable_assignments:
-            self._add_top_k_source(second_predicate, possible_assignment)
+        self.edb.add_source(first_predicate, first_predicate_embd)       
+        first_predicate_embd = self._add_emb_source(first_predicate)
+
+        self._add_rules(first_predicate,  first_subject)
+        subject = get_py_format(first_subject)
+        intermediate_variables = first_predicate_embd.get_iterator(subject, last_step=False)
         
+        self._add_emb_source(second_predicate)
         
+        for intermediate_result in intermediate_variables.data:
+            # second_predicate_embd = EmbTopKEDBTable(second_predicate, second_predicate, 14505, self.edb, self.embedding_model, self.dataset.entity_dict, score_threshold=self.threshold)
+            # self.edb.add_source(second_predicate, second_predicate_embd) 
+            _, intermediate_object, probabilty = intermediate_result
+            # print("intermediate", self.dataset.entitites_to_name[intermediate_object.strip("<>")], probapPPbilty)
+
+            self._add_rules(second_predicate, intermediate_object)
+        
+
     def _handle_2p(self, query):
-
-        self._handle_conjunction(query.first_predicate_query, query.first_subject_query, query.second_predicate_query)
+        self._handle_conjunction(query.first_predicate_query, query.first_subject_query , query.second_predicate_query)
         return query
 
     def _handle_3p(self, query):
-        self._add_top_k_source(query.first_predicate_query, query.first_subject_query)
-        intermediate_variable_assignments = self._collect_top_k_intermediate_variable_assignments(query.first_predicate_query, query.first_subject_query)
-        for possible_assignment in intermediate_variable_assignments:
-            self._add_top_k_source(query.second_predicate_query, possible_assignment)
-            intermediate_variable_assignments_2 = self._collect_top_k_intermediate_variable_assignments(query.first_predicate_query, query.first_subject_query)
-            for possible_assignment in intermediate_variable_assignments_2:
-                self._add_top_k_source(query.third_predicate_query, possible_assignment)
+        first_predicate = self._add_emb_source(query.first_predicate_query)
+        self._add_rules(query.first_predicate_query,  query.first_subject_query)
+        first_subject = get_py_format(query.first_subject_query)
+        intermediate_variables = first_predicate.get_iterator(first_subject, last_step=False)
+        second_predicate = self._add_emb_source(query.second_predicate_query)
+        for intermediate_result in intermediate_variables.data:
+            _, intermediate_object, probabilty = intermediate_result
+            self._add_rules(query.second_predicate_query, intermediate_object)
+
+            intermediate_py_object = get_py_format(intermediate_object)
+            intermediate_variables_2 = second_predicate.get_iterator(intermediate_py_object, last_step=False)
+            for intermediate_result in intermediate_variables_2.data:
+                _, intermediate_object, probabilty = intermediate_result
+                self._add_rules(query.second_predicate_query, intermediate_object)
+
 
         return query
+
 
 
     def _handle_2i(self, query):
-        self._handle_atom(query.first_predicate_rule_1, query.first_subject_rule_1)
-        self._handle_atom(query.first_predicate_rule_2, query.first_subject_rule_2)
+
+        self._add_emb_source(query.first_predicate_rule_1)
+        self._add_rules(query.first_predicate_rule_1, query.first_subject_rule_1)
+
+        self._add_emb_source(query.first_predicate_rule_2)
+        self._add_rules(query.first_predicate_rule_2, query.first_subject_rule_2)
+
+    
         return query
 
     def _handle_3i(self, query):
-        self._handle_atom(query.first_predicate_rule_1, query.first_subject_rule_1)
-        self._handle_atom(query.first_predicate_rule_2, query.first_subject_rule_2)
-        self._handle_atom(query.first_predicate_rule_3, query.first_subject_rule_3)
+        self._add_emb_source(query.first_predicate_rule_1)
+        self._add_rules(query.first_predicate_rule_1,   query.first_subject_rule_1)
+
+        self._add_emb_source(query.first_predicate_rule_2)
+        self._add_rules(query.first_predicate_rule_2,   query.first_subject_rule_2)
+
+        self._add_emb_source(query.first_predicate_rule_3)
+        self._add_rules(query.first_predicate_rule_3,   query.first_subject_rule_3)
         return query
     
     def _handle_ci(self, query):
         self._handle_conjunction(query.first_predicate_rule_1, query.first_subject_rule_1, query.second_predicate_rule_1)
-        self._handle_atom(query.first_predicate_rule_2, query.first_subject_rule_2)
+        
+        self._add_emb_source(query.first_predicate_rule_2)
+        self._add_rules(query.first_predicate_rule_2,   query.first_subject_rule_2)
+
         return query
         
     def _handle_ic(self, query):
@@ -390,28 +409,7 @@ class ApproximateProbabilisticQueryAnswerer():
     def _add_deterministic_facts_to_knowledge_base(self):
         self.program.add_rule("PKB(X, Y, Z) :- TE(X, Y, Z)")
      
-   
-    def _add_top_k_source(self, predicate, subject):
-        E = EmbTopKEDBTable(predicate, predicate, self.k, self.edb, self.embedding_model, self.dataset.entity_dict)
 
-        # if source exists skip
-        predicates_in_edb = self.edb.get_predicates()
-        if not predicate in predicates_in_edb:
-        
-            self.edb.add_source(predicate, E)
-
+    def _add_rules(self, predicate, subject):
         self.rules.append(f"{predicate}_{subject}(X, S) :- {predicate}({subject},X,S)")
         self.rules.append(f"PKB({subject},{predicate}, X) :- {predicate}_{subject}(X,S)")
-        
-
-    def _collect_top_k_intermediate_variable_assignments(self, predicate,subject):
-        temp_predicate_name = f"{predicate}_{subject}"
-        # update the program with the new edb source
-        self.program = self.edb
-        self._add_probabilistic_facts_to_knowledge_base()
-        # create a (temp) engine just to get the intermediate variables (CQ-BEAM style)
-        engine = Engine(self.edb, self.program)
-        intermediate_variables = engine.get_facts_in_predicate(temp_predicate_name)
-        return intermediate_variables
-      
-    

src/metrics.py
@@ -5,11 +5,10 @@ import logging
 
 
 
-def evaluation(scores, answers, dataset):
+def evaluation(scores, answers, dataset, max_queries):
     '''This function is written by Daniel Daza (CQD) '''
     queries, answers, answers_hard = answers
 
-    scores = torch.transpose(scores,0,1)
     nentity = len(scores[0])
     step = 0
     logs = []
@@ -18,32 +17,29 @@ def evaluation(scores, answers, dataset):
     for query_id, query in enumerate(tqdm(queries)):
 
         
+ 
 
-        if query_id == 3:
+        if query_id == max_queries:
             break
-        anchor, rel1, x1, x2, rel2, x3 = query.split('_')
-        anchor = dataset.index_to_entities[int(anchor)]
-        rel1 = dataset.index_to_relation[int(rel1)]
-        rel2 = dataset.index_to_relation[int(rel2)]
-        # print(
-        #     f'Query {query_id}: ?Y:∃ X.({anchor}, {rel1}, X) and (X, {rel2}, Y)')
+     
 
         score = scores[query_id]
-        # print(score.shape)
-        # print(score)
-  
-        score -= (torch.min(score) - 1)
+        score = scores
+        # score -= (torch.min(score) - 1)
+
+
         ans = answers[query]
-        # print("Score", score)
         hard_ans = answers_hard[query]
-        # print("HARD ANS", hard_ans)
+
+       
         all_idx = set(range(nentity))
 
         false_ans = all_idx - set(ans)
         ans_list = list(ans)
         hard_ans_list = list(hard_ans)
         hard_ans_txt = [dataset.index_to_entities[int(i)] for i in hard_ans_list]
-        # print("hard ands txt", hard_ans_txt)
+        ans_txt = [dataset.index_to_entities[int(i)] for i in ans_list]
+        
         false_ans_list = list(false_ans)
         ans_idxs = np.array(hard_ans_list)
         vals = np.zeros((len(ans_idxs), nentity))
@@ -59,10 +55,12 @@ def evaluation(scores, answers, dataset):
         vals[axis1, axis2] = 1
         b = torch.tensor(vals, device=scores.device)
         filter_score = b * score
+
         argsort = torch.argsort(filter_score, dim=1, descending=True)
         ans_tensor = torch.tensor(hard_ans_list, device=scores.device, dtype=torch.long)
         argsort = torch.transpose(torch.transpose(argsort, 0, 1) - ans_tensor, 0, 1)
         ranking = (argsort == 0).nonzero(as_tuple=False)
+    
         ranking = ranking[:, 1]
         ranking = ranking + 1
         ans_vec = np.zeros(nentity)
@@ -73,14 +71,24 @@ def evaluation(scores, answers, dataset):
         mrm = torch.mean(ranking.to(torch.float)).item()
         mrrm = torch.mean(1./ranking.to(torch.float)).item()
         num_ans = len(hard_ans_list)
+ 
 
         hits1m_newd = hits1m
         hits3m_newd = hits3m
         hits10m_newd = hits10m
         mrm_newd = mrm
         mrrm_newd = mrrm
-
+ 
         logs.append({
+            'MRRm_new': mrrm_newd,
+            'MRm_new': mrm_newd,
+            'HITS@1m_new': hits1m_newd,
+            'HITS@3m_new': hits3m_newd,
+            'HITS@10m_new': hits10m_newd,
+            'num_answer': num_ans
+        })
+
+        print({
             'MRRm_new': mrrm_newd,
             'MRm_new': mrm_newd,
             'HITS@1m_new': hits1m_newd,